e41ea7e7f9641f73871ed00c9316ee5e1410fdf9
Wen Zhao (Joan) <withzhaowen@126.com>
2022-06-19 12:14:59 +0800

    feat Synchronized 同步锁

---
+++ src/main/java/multithreaded/SynchronizedExample2.java
@@ -0,0 +1,17 @@
+package multithreaded;
+
+/**
+ * @author zhao wen
+ * @sice 2022/6/19
+ **/
+public class SynchronizedExample2 {
+    // 修饰普通方法
+    public synchronized void method1() {
+        // code
+    }
+
+    // 修饰静态方法
+    public  synchronized static void method2() {
+        // code
+    }
+}
